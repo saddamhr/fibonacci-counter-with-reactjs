f3e4c4513d26a5f099317a7dd57a58e78e3d4ad4
saddamhr <saddambubt65@gmail.com>
2020-08-14 21:18:56 +0600

    setup project structure

---
+++ src/components/Fibonacci/Fibonacci.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Fibonacci.module.css';
+
+const Fibonacci = () => {
+    return (
+        <h1>Fibonacci</h1>
+    )
+}
+
+export default Fibonacci;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Fibonacci } from './Fibonacci/Fibonacci';
+// export { default as Cards } from './Cards/Cards';
