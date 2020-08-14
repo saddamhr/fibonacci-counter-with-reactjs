0ba1afbdb9d7e17b334f389d54b9b5ca8feb0610
saddamhr <saddambubt65@gmail.com>
2020-08-14 22:22:44 +0600

    button disabled added

--- src/components/Fibonacci/Fibonacci.jsx
+++ src/components/Fibonacci/Fibonacci.jsx
@@ -7,7 +7,11 @@ const Fibonacci = (props) => {
 
         <div className={styles.DivCenter}>
             <p className={styles.CounterOutput}>The current Fibonacci number: {props.data}</p>
-            <button className={styles.CounterControl} onClick={props.decrease}>Decrement</button>
+            {
+                props.data === 1 ?
+                    <button className={styles.CounterControl} disabled={true} onClick={props.decrease}>Decrement</button> :
+                    <button className={styles.CounterControl} onClick={props.decrease}>Decrement</button>
+            }
             <button className={styles.CounterControl} onClick={props.increase}>Increment</button>
         </div>
     )
